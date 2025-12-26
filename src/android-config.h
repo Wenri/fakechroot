e9675707e367c8cae9684f0781c6a6efb4955e13
Bingchen Gong <6704443+Wenri@users.noreply.github.com>
2025-12-26 12:26:30 +0100

    Simplify: use ANDROID_* macros directly
    Remove FAKECHROOT_ANDROID_* prefix - just use ANDROID_* directly.
    Nix build passes -DANDROID_ELFLOADER etc.

--- src/android-config.h
+++ src/android-config.h
@@ -3,43 +3,38 @@
     Android-specific configuration for nix-on-droid
 
     All paths are compile-time constants defined via -D flags:
-      -DFAKECHROOT_ANDROID_ELFLOADER="..."
-      -DFAKECHROOT_ANDROID_LIBRARY_PATH="..."
-      -DFAKECHROOT_ANDROID_PRELOAD="..."
-      -DFAKECHROOT_ANDROID_BASE="..."
-      -DFAKECHROOT_ANDROID_EXCLUDE_PATH="..."
+      -DANDROID_ELFLOADER="..."
+      -DANDROID_LIBRARY_PATH="..."
+      -DANDROID_PRELOAD="..."
+      -DANDROID_BASE="..."
+      -DANDROID_EXCLUDE_PATH="..."
 */
 
 #ifndef __ANDROID_CONFIG_H
 #define __ANDROID_CONFIG_H
 
 /* Validate required compile-time definitions */
-#ifndef FAKECHROOT_ANDROID_ELFLOADER
-#error "FAKECHROOT_ANDROID_ELFLOADER must be defined at compile time"
+#ifndef ANDROID_ELFLOADER
+#error "ANDROID_ELFLOADER must be defined at compile time"
 #endif
 
-#ifndef FAKECHROOT_ANDROID_LIBRARY_PATH
-#error "FAKECHROOT_ANDROID_LIBRARY_PATH must be defined at compile time"
+#ifndef ANDROID_LIBRARY_PATH
+#error "ANDROID_LIBRARY_PATH must be defined at compile time"
 #endif
 
-#ifndef FAKECHROOT_ANDROID_PRELOAD
-#error "FAKECHROOT_ANDROID_PRELOAD must be defined at compile time"
+#ifndef ANDROID_PRELOAD
+#error "ANDROID_PRELOAD must be defined at compile time"
 #endif
 
-#ifndef FAKECHROOT_ANDROID_BASE
-#error "FAKECHROOT_ANDROID_BASE must be defined at compile time"
+#ifndef ANDROID_BASE
+#error "ANDROID_BASE must be defined at compile time"
 #endif
 
-#ifndef FAKECHROOT_ANDROID_EXCLUDE_PATH
-#error "FAKECHROOT_ANDROID_EXCLUDE_PATH must be defined at compile time"
+#ifndef ANDROID_EXCLUDE_PATH
+#error "ANDROID_EXCLUDE_PATH must be defined at compile time"
 #endif
 
-/* Use macros directly - string literals are deduplicated by linker */
-#define ANDROID_ELFLOADER       FAKECHROOT_ANDROID_ELFLOADER
-#define ANDROID_LIBRARY_PATH    FAKECHROOT_ANDROID_LIBRARY_PATH
-#define ANDROID_PRELOAD         FAKECHROOT_ANDROID_PRELOAD
-#define ANDROID_BASE            FAKECHROOT_ANDROID_BASE
-#define ANDROID_EXCLUDE_PATH    FAKECHROOT_ANDROID_EXCLUDE_PATH
-#define ANDROID_ARGV0_OPT       "--argv0"
+/* Hardcoded --argv0 option for login shell detection */
+#define ANDROID_ARGV0_OPT "--argv0"
 
 #endif /* __ANDROID_CONFIG_H */
